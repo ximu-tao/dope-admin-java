684a2122ee51bcf67a99dd94add3e75b2d1f5cf4
ximu-tao <me_litao@outlook.com>
2025-04-19 19:57:40 +0800

    通用的第三方绑定信息

---
+++ src/main/java/com/cool/modules/user/entity/UserOauthEntity.java
@@ -0,0 +1,34 @@
+package com.cool.modules.user.entity;
+
+import com.cool.core.base.AppEntity;
+import com.cool.core.base.UserEntity;
+import com.mybatisflex.annotation.Table;
+import com.tangzc.mybatisflex.autotable.annotation.ColumnDefine;
+import com.tangzc.mybatisflex.autotable.annotation.UniIndex;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import org.dromara.autotable.annotation.Index;
+
+@Data
+@Table(value = "user_oauth", comment = "第三方绑定信息")
+@Schema( description = "第三方绑定信息")
+public class UserOauthEntity extends AppEntity<UserOauthEntity> implements UserEntity {
+    
+    @ColumnDefine(comment = "厂商", notNull = true)
+    private String provider;
+    
+    @ColumnDefine(comment = "平台", notNull = true)
+    private String platform;
+
+    @Index
+    @ColumnDefine(comment = "第三方unionid")
+    private String unionid;
+
+    @UniIndex
+    @ColumnDefine(comment = "第三方openid", notNull = true)
+    private String openid;
+
+
+    @ColumnDefine(comment = "其他扩展数据" , type = "varchar(512)")
+    private String extend;
+}
