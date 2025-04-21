90ab94eb7fcfc9aac79c613a596455bb03af2ce4
ximu-tao <me_litao@outlook.com>
2025-04-21 16:33:51 +0800

    VIP开通规则表

---
+++ src/main/java/com/cool/modules/user/entity/UserVipInfoEntity.java
@@ -0,0 +1,26 @@
+package com.cool.modules.user.entity;
+
+import com.cool.core.base.BaseEntity;
+import com.mybatisflex.annotation.Table;
+import com.tangzc.mybatisflex.autotable.annotation.ColumnDefine;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Data
+@Table(value = "user_vip_info" , comment = "会员开通配置")
+@Schema( description = "会员开通配置")
+public class UserVipInfoEntity extends BaseEntity<UserVipInfoEntity> {
+    
+    @ColumnDefine(comment = "价格", notNull = true)
+    private Double price;
+    
+    @ColumnDefine(comment = "原价" )
+    private Double oldPrice;
+    
+    @ColumnDefine(comment = "标题" )
+    private String title;
+    
+    @ColumnDefine(comment = "会员天数" )
+    private Integer day;
+    
+}
