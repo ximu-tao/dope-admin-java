cdf9264a608f99ea97aec7a518cc760682e41758
ximu-tao <me_litao@outlook.com>
2025-04-22 21:48:54 +0800

    优化接口文档

--- src/main/java/com/cool/modules/user/controller/app/AppUserInfoController.java
+++ src/main/java/com/cool/modules/user/controller/app/AppUserInfoController.java
@@ -1,6 +1,5 @@
 package com.cool.modules.user.controller.app;
 
-import cn.hutool.json.JSONObject;
 import com.cool.core.annotation.CoolRestController;
 import com.cool.core.request.R;
 import com.cool.core.util.CoolSecurityUtil;
@@ -8,11 +7,15 @@ import com.cool.core.util.EntityUtils;
 import com.cool.modules.user.entity.UserInfoEntity;
 import com.cool.modules.user.service.UserInfoService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RequiredArgsConstructor
 @Tag(name = "用户信息", description = "用户信息")
@@ -32,22 +35,36 @@ public class AppUserInfoController {
 
     @Operation(summary = "更新用户信息")
     @PostMapping("/updatePerson")
-    public R updatePerson(@RequestAttribute JSONObject requestParams) {
-        UserInfoEntity infoEntity = requestParams.toBean(UserInfoEntity.class);
+    public R updatePerson(@RequestBody UserInfoEntity infoEntity ) {
         infoEntity.setId(CoolSecurityUtil.getCurrentUserId());
+        
+        infoEntity.setPassword(null);
+        infoEntity.setPassword(null);
+        infoEntity.setStatus(null);
+        
         return R.ok(
             userInfoService.updateById(infoEntity)
         );
     }
+    
+    
+    @Data
+    public static class UpdateUserInfo{
+        
+        @Schema( description = "密码")
+        private String password;
+        
+        @Schema( description = "手机号")
+        private String phone;
+        
+        @Schema( description = "验证码")
+        private String code;
+    }
 
     @Operation(summary = "更新用户密码")
     @PostMapping("/updatePassword")
-    public R updatePassword(
-        @RequestAttribute JSONObject requestParams
-    ) {
-        String password = requestParams.get("password", String.class);
-        String code = requestParams.get("code", String.class);
-        userInfoService.updatePassword(CoolSecurityUtil.getCurrentUserId(), password, code);
+    public R updatePassword(@Valid @RequestBody UpdateUserInfo updateUserInfo) {
+        userInfoService.updatePassword(CoolSecurityUtil.getCurrentUserId(), updateUserInfo.getPassword() , updateUserInfo.getCode() );
         return R.ok();
     }
 
@@ -60,11 +77,8 @@ public class AppUserInfoController {
 
     @Operation(summary = "绑定手机号")
     @PostMapping("/bindPhone")
-    public R bindPhone(
-        @RequestAttribute JSONObject requestParams) {
-        String phone = requestParams.get("phone", String.class);
-        String code = requestParams.get("code", String.class);
-        userInfoService.bindPhone(CoolSecurityUtil.getCurrentUserId(), phone, code);
+    public R bindPhone( @Valid @RequestBody UpdateUserInfo updateUserInfo ) {
+        userInfoService.bindPhone(CoolSecurityUtil.getCurrentUserId(), updateUserInfo.getPhone() , updateUserInfo.getCode());
         return R.ok();
     }
 }
--- src/main/java/com/cool/modules/user/controller/app/params/LoginParam.java
+++ src/main/java/com/cool/modules/user/controller/app/params/LoginParam.java
@@ -1,5 +1,6 @@
 package com.cool.modules.user.controller.app.params;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,16 +9,21 @@ import lombok.Setter;
 public class LoginParam {
 
     /*******小程序/公众号/微信APP授权 登录*******/
+    @Schema(description = "（小程序/公众号/微信APP授权）")
     private String code;
 
+    @Schema(description = "（小程序/公众号/微信APP授权）")
     private String encryptedData;
 
+    @Schema(description = "（小程序/公众号/微信APP授权）")
     private String iv;
 
 
     /*******手机号登录*******/
+    @Schema(description = "手机号（验证码登录/密码登录）")
     private String phone;
 
+    @Schema(description = "验证码（验证码登录）")
     private String smsCode;
 
 
@@ -30,5 +36,6 @@ public class LoginParam {
 
 
     /*******密码登录*******/
+    @Schema(description = "密码,密码登录")
     private String password;
 }
