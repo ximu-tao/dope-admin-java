cbdec8160d5671d34bcd8a75d66c2e3f0fa2216d
ximu-tao <me_litao@outlook.com>
2025-04-28 14:30:27 +0800

    接口文档优化

--- src/main/java/com/cool/modules/user/entity/UserAddressEntity.java
+++ src/main/java/com/cool/modules/user/entity/UserAddressEntity.java
@@ -6,6 +6,7 @@ import com.cool.core.base.BaseEntity;
 import com.cool.core.base.BelongingUserEntity;
 import com.mybatisflex.annotation.Table;
 import com.tangzc.mybatisflex.autotable.annotation.ColumnDefine;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import org.dromara.autotable.annotation.Index;
@@ -16,33 +17,42 @@ import org.dromara.autotable.annotation.Index;
 @Getter
 @Setter
 @Table(value = "user_address", comment = "用户模块-收货地址")
+@Schema( description = "用户收货地址" )
 public class UserAddressEntity extends BaseEntity<UserAddressEntity> implements BelongingUserEntity {
 
     @Index
     @ColumnDefine(comment = "用户ID", notNull = true)
     @EspRemoteSelectField( titleField = "nickName", clazz = UserInfoEntity.class)
     @EpsField( immutable = true )
+    @Schema( description = "用户ID" )
     private Long userId;
 
     @ColumnDefine(comment = "联系人", notNull = true)
+    @Schema( description = "联系人" )
     private String contact;
 
     @Index
     @ColumnDefine(comment = "手机号", length = 11, notNull = true)
+    @Schema( description = "手机号" )
     private String phone;
 
     @ColumnDefine(comment = "省", notNull = true)
+    @Schema( description = "省" )
     private String province;
 
     @ColumnDefine(comment = "市", notNull = true)
+    @Schema( description = "市" )
     private String city;
 
     @ColumnDefine(comment = "区", notNull = true)
+    @Schema( description = "区" )
     private String district;
 
     @ColumnDefine(comment = "地址", notNull = true)
+    @Schema( description = "地址" )
     private String address;
 
     @ColumnDefine(comment = "是否默认", defaultValue = "false")
+    @Schema( description = "是否默认" )
     private Boolean isDefault;
 }
