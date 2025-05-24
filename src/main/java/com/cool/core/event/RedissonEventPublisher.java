b90e5bbaae30d2654ea3491ed2e8fd079a4ad347
ximu-tao <me_litao@outlook.com>
2025-05-24 21:31:21 +0800

    日志

--- src/main/java/com/cool/core/event/RedissonEventPublisher.java
+++ src/main/java/com/cool/core/event/RedissonEventPublisher.java
@@ -1,5 +1,6 @@
 package com.cool.core.event;
 
+import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RTopic;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 /**
  * Redisson事件发布器实现（支持自定义事件名）
  */
+@Slf4j
 @Component
 public class RedissonEventPublisher implements EventPublisher {
 
@@ -20,6 +22,7 @@ public class RedissonEventPublisher implements EventPublisher {
 
     @Override
     public void publish(String eventName, Object event) {
+        log.info( "EVENT {} : {}" , eventName , event  );
         RTopic topic = redissonClient.getTopic(eventName);
         topic.publish(event);
     }
